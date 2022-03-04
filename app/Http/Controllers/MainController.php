30ceedd45208347f25f4197ae95aa6f0981b0e4f
jibundeyare <contact@jibundeyare.com>
2022-03-04 15:05:30 +0100

    Test de tri par ordre alpha

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -22,9 +22,15 @@ class MainController extends Controller
 
     public function test()
     {
-        $plats = Plat::all();
+        // récupère tous les plats
+        // $plats = Plat::all();
+
+        // récupère tous les plats, en les triant par ordre alpha de la colonne "nom"
+        $plats = Plat::select()->orderBy('nom')->get();
 
         foreach ($plats as $plat) {
+            dump('# le plat');
+
             dump($plat->id);
             dump($plat->nom);
             // dump($plat->description);
@@ -32,21 +38,36 @@ class MainController extends Controller
 
             $categorie = $plat->categorie()->first();
 
+            dump('## la catégorie');
             dump($categorie->id);
             dump($categorie->nom);
             dump($categorie->description);
+
+            $etiquettes = $plat->etiquettes()->orderBy('nom')->get();
+
+            dump('## les étiquettes');
+
+            foreach ($etiquettes as $etiquette) {
+                dump($etiquette->id);
+                dump($etiquette->nom);
+                dump($etiquette->description);
+            }
         }
 
         exit();
 
-        $etiquettes = Etiquette::all();
+        // récupère toutes les étiquettes
+        // $etiquettes = Etiquette::all();
+
+        // récupère toutes les étiquettes, en les triant par ordre alpha de la colonne "nom"
+        $etiquettes = Etiquette::select()->orderBy('nom')->get();
 
         foreach ($etiquettes as $etiquette) {
             dump($etiquette->id);
             dump($etiquette->nom);
             dump($etiquette->description);
 
-            $plats = $etiquette->plats()->get();
+            $plats = $etiquette->plats()->orderBy('nom')->get();
 
             foreach ($plats as $plat) {
                 dump($plat->id);
@@ -58,17 +79,21 @@ class MainController extends Controller
 
         exit();
 
-        $categories = Categorie::all();
+        // récupère toutes les catégories
+        // $categories = Categorie::all();
+
+        // récupère toutes les catégories, en les triant par ordre alpha de la colonne "nom"
+        $categories = Categorie::select()->orderBy('nom')->get();
 
         foreach ($categories as $categorie) {
-            // enregistre les données dans le fichier `storage/logs/laravel.log`
+            // enregistre les données dans le fichier 'storage/logs/laravel.log'
             // Log::debug($categorie);
 
             dump($categorie->id);
             dump($categorie->nom);
             dump($categorie->description);
 
-            $plats = $categorie->plats()->get();
+            $plats = $categorie->plats()->orderBy('nom')->get();
 
             foreach ($plats as $plat) {
                 dump($plat->id);
