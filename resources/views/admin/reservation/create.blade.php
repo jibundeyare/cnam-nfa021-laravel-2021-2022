3248ac2840b20ca4a575bd5fe9f643f073f83ef9
jibundeyare <contact@jibundeyare.com>
2022-03-17 14:25:01 +0100

    Ajout de code de déboggage de formulaire

--- resources/views/admin/reservation/create.blade.php
+++ resources/views/admin/reservation/create.blade.php
@@ -9,6 +9,13 @@
         <div class="col">
             <h1>Admin - Création réservation</h1>
 
+            {{-- code utile pour débogger un formulaire et une validation qui ne fonctionnent pas correctement --}}
+            {{--
+            @foreach ($errors->all() as $error)
+              <div>{{ $error }}</div>
+            @endforeach
+            --}}
+
             <form action="{{ route('admin.reservation.store') }}" method="post">
                 @csrf
                 <div>
